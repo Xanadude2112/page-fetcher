71df365741548711373df07ea8a46a0fd9b062d3
Thomas Azran <tommy.azran@gmail.com>
2024-02-22 23:02:53 +0000

    get rid of comments

--- fetcher.js
+++ fetcher.js
@@ -1,29 +1,3 @@
-// const net = require('net');
-// const conn = net.createConnection({
-//   host: 'example.edu',
-//   port: 80
-// });
-// conn.setEncoding('UTF8');
-
-// //This code is making a valid HTTP 1.1 request using the minimum number of possible "words". Here is the text being sent to the server.
-// conn.on('connect', () => {
-//   console.log(`Connected to server!`);
-
-//   // conn.write(`GET / HTTP/1.1\r\n`);
-//   // conn.write(`Host: example.edu\r\n`);
-//   // conn.write(`\r\n`);
-// });
-
-// /**
-//  * HTTP Response
-//  * After request is made, the HTTP server should send us HTTP data via our TCP connection
-//  * Print the data to the screen, and end the connection
-//  */
-// conn.on('data', (data) => {
-//   console.log(data);
-//   conn.end();
-// });
-
 const fs = require("fs");
 const request = require("request");
 
